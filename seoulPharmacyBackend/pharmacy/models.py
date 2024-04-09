3e08be16ea20af4d71d912213924daecf9848289
hl3470 <hl3470@naver.com>
2024-04-10 02:18:30 +0900

    feat: Pharmacy 도메인에 기본키 명시

--- seoulPharmacyBackend/pharmacy/models.py
+++ seoulPharmacyBackend/pharmacy/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
-
 class Pharmacy(models.Model):
+    id = models.AutoField(primary_key=True, db_column='pharmacy_id')
+
     name = models.CharField(max_length=200, blank=False, null=False)
 
     si = models.CharField(max_length=10, blank=False, null=False)
@@ -13,25 +14,28 @@ class Pharmacy(models.Model):
     latitude = models.DecimalField(max_digits=9, decimal_places=6, blank=False, null=False)
     longitude = models.DecimalField(max_digits=9, decimal_places=6, blank=False, null=False)
 
-    speaking_english = models.BooleanField(null=False)
-    speaking_japanese = models.BooleanField(null=False)
-    speaking_chinese = models.BooleanField(null=False)
+    speaking_english = models.BooleanField(null=False, default=False)
+    speaking_japanese = models.BooleanField(null=False, default=False)
+    speaking_chinese = models.BooleanField(null=False, default=False)
 
-    mon_open_time = models.TimeField
-    tue_open_time = models.TimeField
-    wed_open_time = models.TimeField
-    thu_open_time = models.TimeField
-    fri_open_time = models.TimeField
-    sat_open_time = models.TimeField
-    sun_open_time = models.TimeField
-    holiday_open_time = models.TimeField
+    mon_open_time = models.TimeField(null=True)
+    tue_open_time = models.TimeField(null=True)
+    wed_open_time = models.TimeField(null=True)
+    thu_open_time = models.TimeField(null=True)
+    fri_open_time = models.TimeField(null=True)
+    sat_open_time = models.TimeField(null=True)
+    sun_open_time = models.TimeField(null=True)
+    holiday_open_time = models.TimeField(null=True)
 
-    mon_close_time = models.TimeField
-    tue_close_time = models.TimeField
-    wed_close_time = models.TimeField
-    thu_close_time = models.TimeField
-    fri_close_time = models.TimeField
-    sat_close_time = models.TimeField
-    sun_close_time = models.TimeField
-    holiday_close_time = models.TimeField
+    mon_close_time = models.TimeField(null=True)
+    tue_close_time = models.TimeField(null=True)
+    wed_close_time = models.TimeField(null=True)
+    thu_close_time = models.TimeField(null=True)
+    fri_close_time = models.TimeField(null=True)
+    sat_close_time = models.TimeField(null=True)
+    sun_close_time = models.TimeField(null=True)
+    holiday_close_time = models.TimeField(null=True)
+
+    def __str__(self):
+        return self.name
 
