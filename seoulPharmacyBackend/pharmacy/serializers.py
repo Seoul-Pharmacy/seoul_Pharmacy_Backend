86f00dd22006547e439b24d086aa0aa487637902
hl3470 <hl3470@naver.com>
2024-04-11 12:55:17 +0900

    feat: pharmacy crud 기능 구현
    - Phatmacy 저장하기
    - 전체 Pharmacy 정보 가져오기
    - 특정 Pharmacy 정보 가져오기
    - 특정 Pharmacy 삭제하기
    - 특정 Pharmacy 변경하기

--- seoulPharmacyBackend/pharmacy/serializers.py
+++ seoulPharmacyBackend/pharmacy/serializers.py
@@ -8,31 +8,9 @@ class PharmacySerializer(serializers.ModelSerializer):
         model = Pharmacy
         fields = '__all__'
 
-    name = serializers.CharField()
-
-    def create(self, validated_data):
-        return Pharmacy.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('name', instance.title)
-        instance.save()
-        return instance
-
 
 class SimplePharmacySerializer(serializers.ModelSerializer):
     class Meta:
         model = Pharmacy
-        fields = '__all__'
-
-    id = serializers.IntegerField()
-    
-    name = serializers.CharField()
-
-
-    def create(self, validated_data):
-        return Pharmacy.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('name', instance.title)
-        instance.save()
-        return instance
+        fields = ['id', 'name', 'si', 'gu', 'roadNameAddress', 'speaking_english', 'speaking_japanese',
+                  'speaking_chinese']
--- seoulPharmacyBackend/pharmacy/views.py
+++ seoulPharmacyBackend/pharmacy/views.py
@@ -7,15 +7,52 @@ from .serializers import PharmacySerializer
 
 
 @api_view(['GET'])
-def PharmacyList(request):
-    queryset = Pharmacy.objects.all()
-    serializer = PharmacySerializer(queryset, many=True)
+def pharmacyList(request) -> Response:
+    pharmacies = Pharmacy.objects.all()
+    serializer = PharmacySerializer(pharmacies, many=True)
     return Response(serializer.data)
 
-@api_view(['GET'])
-def pharmacyDetails(request, id):
-    pharmacy = Pharmacy.objects.get(id=id)
-    serializer = PharmacySerializer(pharmacy)
-    return Response(serializer.data)
 
-# Create your views here.
+@api_view(['POST'])
+def pharmacySave(request) -> Response:
+    serializer = PharmacySerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def pharmacyDetails(request, id) -> Response:
+    if request.method == 'GET':
+        pharmacy = Pharmacy.objects.get(id=id)
+        serializer = PharmacySerializer(pharmacy)
+        return Response(serializer.data)
+
+    if request.method == 'PUT':
+        try:
+            query = Pharmacy.objects.get(id=id)
+        except Pharmacy.DoesNotExist as e:
+            return Response({'error': {
+                'code': 404,
+                'message': "Pharmacy not found!"
+            }}, status=status.HTTP_404_NOT_FOUND)
+
+        pharmacy = PharmacySerializer(query, data=request.data)
+        if pharmacy.is_valid():
+            pharmacy.save()
+            return Response(pharmacy.data)
+        return Response(pharmacy.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method == 'DELETE':
+        try:
+            query = Pharmacy.objects.get(id=id)
+        except Pharmacy.DoesNotExist:
+            return Response({'error': {
+                'code': 404,
+                'message': "User not found!"
+            }}, status=status.HTTP_404_NOT_FOUND)
+
+        query.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
