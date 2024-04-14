910d0e5d889da6638c69bec19fbda8a0e5c7ae26
hl3470 <hl3470@naver.com>
2024-04-14 22:35:17 +0900

    feat: 모든 url에 /api 추가

--- seoulPharmacyBackend/seoulPharmacyBackend/urls.py
+++ seoulPharmacyBackend/seoulPharmacyBackend/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('pharmacy.urls')),
+    path('api/', include('pharmacy.urls')),
     path('admin/', admin.site.urls),
 ]
