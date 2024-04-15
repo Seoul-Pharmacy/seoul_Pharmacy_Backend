9590ef68553fbcef35214a73d5ceca035513c4fd
hl3470 <hl3470@naver.com>
2024-04-15 21:35:53 +0900

    feat: valueError에 대한 핸들링 구현

--- seoulPharmacyBackend/common/custom_exception_handler.py
+++ seoulPharmacyBackend/common/custom_exception_handler.py
@@ -1,7 +1,19 @@
+import logging
+
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.views import exception_handler
+
+logger = logging.getLogger('django')
 
 
 def custom_exception_handler(exc, context):
+    logger.error("valueError : {0}".format(str(exc)))
+
+    if isinstance(exc, ValueError):
+        response_data = {'detail': 'Bad request: Invalid value provided.', 'status_code': status.HTTP_400_BAD_REQUEST}
+        return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
+
     response = exception_handler(exc, context)
 
     if response is not None:
