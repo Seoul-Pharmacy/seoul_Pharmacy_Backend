e511ddf9d0dbc19df54695a7db48015aae00c407
hl3470 <hl3470@naver.com>
2024-04-18 14:32:10 +0900

    feat: 데이터 admin 페이지 구현

--- seoulPharmacyBackend/pharmacy/admin.py
+++ seoulPharmacyBackend/pharmacy/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from pharmacy.models import Pharmacy
+
+
 # Register your models here.
+
+class PharmacyAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Pharmacy, PharmacyAdmin)
