973c65f437958686c4c8c2b075d37969b8b55778
hl3470 <hl3470@naver.com>
2024-04-26 21:03:29 +0900

    refactor: admin페이지에 filter에서 name 제거

--- seoulPharmacyBackend/pharmacy/admin.py
+++ seoulPharmacyBackend/pharmacy/admin.py
@@ -7,8 +7,17 @@ from pharmacy.models import Pharmacy
 
 class PharmacyAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'gu', 'road_name_address']
-    list_filter = ['name', 'gu', 'speaking_english', 'speaking_japanese', 'speaking_chinese']
+    list_filter = ['gu', 'speaking_english', 'speaking_japanese', 'speaking_chinese']
     search_fields = ['name', 'gu', 'road_name_address']
+
+    # def changelist_view(self, request, extra_context=None):
+    #     if (extra_context is None):
+    #         extra_context = {}
+    #     extra_context.update({'buttons': [
+    #         {'name': '약국 운영 시간 전체 업데이트', 'url': '/api/pharmacies/hours'},
+    #         {'name': '약국 외국어 전체 업데이트', 'url': '/api/pharmacies/languages'},
+    #     ]})
+    #     return super().changelist_view(request, extra_context)
 
 
 admin.site.register(Pharmacy, PharmacyAdmin)
