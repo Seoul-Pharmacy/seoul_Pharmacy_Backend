de96bf373da6f43aa25d6314d2b4590b916afdeb
hl3470 <hl3470@naver.com>
2024-04-26 19:05:09 +0900

    feat: admin페이지에 기능추가
    -정렬기능
    -검색기능

--- seoulPharmacyBackend/pharmacy/admin.py
+++ seoulPharmacyBackend/pharmacy/admin.py
@@ -6,7 +6,9 @@ from pharmacy.models import Pharmacy
 # Register your models here.
 
 class PharmacyAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'name', 'gu', 'road_name_address']
+    list_filter = ['name', 'gu', 'speaking_english', 'speaking_japanese', 'speaking_chinese']
+    search_fields = ['name', 'gu', 'road_name_address']
 
 
 admin.site.register(Pharmacy, PharmacyAdmin)
