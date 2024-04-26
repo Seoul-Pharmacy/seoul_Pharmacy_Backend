1fa91de2d68a903bd73b4e512f59250a32f163fb
hl3470 <hl3470@naver.com>
2024-04-27 00:34:04 +0900

    feat: admin 페이지에 업데이트 기능 구현

--- seoulPharmacyBackend/pharmacy/admin.py
+++ seoulPharmacyBackend/pharmacy/admin.py
@@ -10,14 +10,14 @@ class PharmacyAdmin(admin.ModelAdmin):
     list_filter = ['gu', 'speaking_english', 'speaking_japanese', 'speaking_chinese']
     search_fields = ['name', 'gu', 'road_name_address']
 
-    # def changelist_view(self, request, extra_context=None):
-    #     if (extra_context is None):
-    #         extra_context = {}
-    #     extra_context.update({'buttons': [
-    #         {'name': '약국 운영 시간 전체 업데이트', 'url': '/api/pharmacies/hours'},
-    #         {'name': '약국 외국어 전체 업데이트', 'url': '/api/pharmacies/languages'},
-    #     ]})
-    #     return super().changelist_view(request, extra_context)
+    def changelist_view(self, request, extra_context=None):
+        if (extra_context is None):
+            extra_context = {}
+        extra_context.update({'buttons': [
+            {'name': '약국 운영 시간 전체 업데이트', 'url': '/api/pharmacies/hours'},
+            {'name': '약국 외국어 전체 업데이트', 'url': '/api/pharmacies/languages'},
+        ]})
+        return super().changelist_view(request, extra_context)
 
 
 admin.site.register(Pharmacy, PharmacyAdmin)
