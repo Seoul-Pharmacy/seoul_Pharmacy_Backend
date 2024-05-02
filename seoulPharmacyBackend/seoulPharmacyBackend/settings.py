57021deb53b45b59f3660d7094d80e8836969f56
hl3470 <hl3470@naver.com>
2024-05-03 01:50:34 +0900

    refactor: 페이지네이션 한 페이지당 10개에서 5개로 변경

--- seoulPharmacyBackend/seoulPharmacyBackend/settings.py
+++ seoulPharmacyBackend/seoulPharmacyBackend/settings.py
@@ -154,7 +154,7 @@ CORS_ALLOW_HEADERS = (
 REST_FRAMEWORK = {
     'EXCEPTION_HANDLER': 'common.custom_exception_handler.custom_exception_handler',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 5,
 }
 
 # logging
