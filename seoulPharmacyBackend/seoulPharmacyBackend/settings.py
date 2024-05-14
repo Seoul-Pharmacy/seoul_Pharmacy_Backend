fe61728271d511764996e1b8bb0a422fb8c8fc7d
hl3470 <hl3470@naver.com>
2024-05-14 14:34:25 +0900

    fix: admin 관련 페이지들 정정 웹 서버에 전달할 수 있도록 static으로 묶었다.

--- seoulPharmacyBackend/seoulPharmacyBackend/settings.py
+++ seoulPharmacyBackend/seoulPharmacyBackend/settings.py
@@ -18,6 +18,9 @@ from common import my_settings
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# 정적파일 경로
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
